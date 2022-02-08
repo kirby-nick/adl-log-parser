beedbbecbb2d5c86c18d7c8b9fb963c37f6813de
Nick Kirby <kirby_nick@outlook.com>
2022-02-07 23:44:25 -0500

    add IP validation and category

--- app/models/event_log.rb
+++ app/models/event_log.rb
@@ -3,6 +3,29 @@ class EventLog < ApplicationRecord
   validates :body, presence: true
   before_save :parse_ips
 
+  def category(ip)
+    octets = ip.split(".").map(&:to_i)
+    case octets[0]
+    when 10
+      "Internal"
+    when 172
+      octets[1] >= 16 && octets[1] <= 31 ? "Internal" : "External"
+    when 192
+      octets[1] == 168 ? "Internal" : "External"
+    else
+      "External"
+    end
+  end
+
+  def valid_state(ip)
+    if ip.match?(/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/)
+      octects = ip.split(".").map(&:to_i)
+      octects.any? {|oct| oct > 255} ? "Not valid" : "Valid"
+    else
+      "Not valid"
+    end
+  end
+
   private
 
   def parse_ips
